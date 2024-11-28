7274b5eae979a069e45119145d8e123c8fb5316f
gutembergz <31116674+gutembergz@users.noreply.github.com>
2024-11-27 22:49:36 -0300

    Update init.php

--- init.php
+++ init.php
@@ -1,11 +1,11 @@
 <?php
 
 // constantes com as credenciais de acesso ao banco MySQL
-define('DB_HOSTB', 'sh-pro40.hostgator.com.br'); // para uso sem porta
-define('DB_HOST', 'sh-pro40.hostgator.com.br');
-define('DB_USER', 'pote7495_datawork_add1');
-define('DB_PASS', 'iLEz2uYHKPk7Ghc');
-define('DB_NAME', 'pote7495_datawork');
+define('DB_HOSTB', ''); // para uso sem porta
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
 define('DB_PORT', '3306');
 
 // constantes de nome e versão do aplicativo
@@ -19,11 +19,11 @@ define('HEADER_TEMPLATE', __DIR__ . '/include/header.php');
 define('FOOTER_TEMPLATE', __DIR__ . '/include/footer.php');
 
 // constantes de configurações do PHPMailer
-define('MAILER_HOST', 'smtp.kinghost.net');
-define('MAILER_USERNAME', 'editorial@portaldenegocios.com');
-define('MAILER_PASSWORD', 'EbgeEbge12');
-define('MAILER_FROMMAIL', 'editorial@portaldenegocios.com');
-define('MAILER_FROMNAME', 'Portal de Negócios');
+define('MAILER_HOST', '');
+define('MAILER_USERNAME', '');
+define('MAILER_PASSWORD', '');
+define('MAILER_FROMMAIL', '');
+define('MAILER_FROMNAME', '');
 
 // caminho absoluto para a pasta do sistema
 if (!defined('ABSPATH') )
